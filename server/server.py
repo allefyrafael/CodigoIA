59b97307fda5fe2725d1a7ff5cb8f5c7ea662760
Allefy <allefyrafaelsilva@gmail.com>
2024-07-01 16:58:55 -0300

    deu certo!!!!!

--- server/server.py
+++ server/server.py
@@ -12,23 +12,21 @@ def get_location_names():
 
     return response
 
-@app.route('/predict_home_price', methods=['POST'])
+@app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
     bhk = int(request.form['bhk'])
     bath = int(request.form['bath'])
 
-    estimated_price = util.get_estimated_price(location, total_sqft, bhk, bath)
-
     response = jsonify({
-        'estimated_price': estimated_price
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run(host='localhost', port=5000, debug=True)
+    app.run()
